d94fc5c21ec7ad128235aae509cba68686cbe62b
Nenad Pavlovic <nenadp@elfak.rs>
2023-05-19 17:35:48 +0200

    ocenezvezdice

--- Aplikacija/Projektito/client/src/Pages/VozacOcene.js
+++ Aplikacija/Projektito/client/src/Pages/VozacOcene.js
@@ -12,34 +12,60 @@ const VozacOcene = () => {
   const [ocene, setOcene] = useState([]);
   const [ready, setReady] = useState(false);
   const [value, setValue] = React.useState();
+  const itemsPerPage = 2;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const [currentItems, setCurrentItems] = useState([]);
   const config = {
     headers: { Authorization: `Bearer ${Cookies.get("Token")}` },
   };
   useEffect(()=> {
     axios.get(`/Vozac/GetOcene/${user.id}`, config).then((response) => {
-      setOcene(response.data);
+      setTotalPages(Math.ceil(response.data.length / itemsPerPage));
+      setCurrentItems(response.data.slice(indexOfFirstItem, indexOfLastItem));
       setReady(true);
     });
-  },[])
+  },[currentPage,ready])
+  const handleClickNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const handleClickPrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
-    <div className="flex flex-col items-center mt-20">
-      <ul class="w-2/3 divide-y divide-gray-200 dark:divide-gray-700">
+    <div className="flex flex-col items-center mt-2 w-full">
+      <div className="flex justify-center gap-5 mt-10">
+        <button onClick={handleClickPrev} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button onClick={handleClickNext} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+      <ul className="w-2/3 divide-y divide-gray-200 dark:divide-gray-700">
         {ready &&
-          ocene.map((ocena) => (
-            <li class="pb-3 sm:pb-4">
-              <div class="flex items-center space-x-4">
-              <div class="flex-shrink-0">
+          currentItems.map((ocena) => (
+            <li className="pb-3 sm:pb-4" key={ocena.id}>
+              <div className="flex items-center space-x-4">
+              <div className="flex-shrink-0">
                   <p>Slika</p>
               </div>
-                <div class="flex-1 min-w-0">
-                  <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-400">
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-400">
                     Kompanija: {ocena?.kompanija.naziv}
                   </p>
-                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                     Opis: {ocena?.opis}
                   </p>
                 </div>
-                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-gray-400">
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-gray-400">
                   {
                     <Box
                     sx={{
