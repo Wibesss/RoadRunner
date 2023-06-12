c3fc94fbaf6ad400a9d96478727efca7c15c0fbe
DESKTOP-NC1HIU2\Wibes <milanmiljkovic@elfak.rs>
2023-06-12 22:34:06 +0200

    final

--- Aplikacija/Projektito/client/src/Pages/DispecerPrihvaceneTure.js
+++ Aplikacija/Projektito/client/src/Pages/DispecerPrihvaceneTure.js
@@ -140,124 +140,124 @@ const DispecerPrihvaceneTure = () => {
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3 whitespace-nowrap ">
-                <div className="flex flex-row justify-center">
-                  <button
-                    className="flex flex-row uppercase"
-                    onClick={() => sorting("duzinaRobe")}
-                  >
-                    Duzina robe
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1 mb-0"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
+                  <div className="flex flex-row justify-center">
+                    <button
+                      className="flex flex-row uppercase"
+                      onClick={() => sorting("duzinaRobe")}
                     >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </button>
+                      Duzina robe
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1 mb-0"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </button>
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3 whitespace-nowrap ">
-                <div className="flex flex-row justify-center">
-                  <button
-                    className="flex flex-row uppercase"
-                    onClick={() => sorting("sirinaRobe")}
-                  >
-                    Sirina robe
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1 mb-0"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
+                  <div className="flex flex-row justify-center">
+                    <button
+                      className="flex flex-row uppercase"
+                      onClick={() => sorting("sirinaRobe")}
                     >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </button>
+                      Sirina robe
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1 mb-0"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </button>
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3 whitespace-nowrap ">
-                <div className="flex flex-row justify-center">
-                  <button
-                    className="flex flex-row uppercase"
-                    onClick={() => sorting("visinaRobe")}
-                  >
-                    Visina robe
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1 mb-0"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
+                  <div className="flex flex-row justify-center">
+                    <button
+                      className="flex flex-row uppercase"
+                      onClick={() => sorting("visinaRobe")}
                     >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </button>
+                      Visina robe
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1 mb-0"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </button>
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3 whitespace-nowrap">
-                <div className="flex flex-row justify-center">
-                  <button
-                    className="flex flex-row uppercase"
-                    onClick={() => sorting("zapremina")}
-                  >
-                    Zapremina robe
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1 mb-0"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
+                  <div className="flex flex-row justify-center">
+                    <button
+                      className="flex flex-row uppercase"
+                      onClick={() => sorting("zapremina")}
                     >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </button>
+                      Zapremina robe
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1 mb-0"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </button>
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3 whitespace-nowrap">
-                <div className="flex flex-row justify-center">
-                  <button
-                    className="flex flex-row uppercase"
-                    onClick={() => sorting("duzina")}
-                  >
-                    Duzina ture
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1 mb-0"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
+                  <div className="flex flex-row justify-center">
+                    <button
+                      className="flex flex-row uppercase"
+                      onClick={() => sorting("duzina")}
                     >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </button>
+                      Duzina ture
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1 mb-0"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </button>
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3 whitespace-nowrap">
-                <div className="flex flex-row justify-center">
-                  <button
-                    className="flex flex-row uppercase"
-                    onClick={() => sorting("datumPocetka")}
-                  >
-                    Datum pocetka
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1 mb-0"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
+                  <div className="flex flex-row justify-center">
+                    <button
+                      className="flex flex-row uppercase"
+                      onClick={() => sorting("datumPocetka")}
                     >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </button>
+                      Datum pocetka
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1 mb-0"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </button>
                   </div>
                 </th>
-                <th scope="col" className="px-6 py-3 whitespace-nowrap flex justify-center">
-                  Prikazi
+                <th scope="col" className="px-6 py-3 whitespace-nowrap ">
+                  <div className="flex justify-center"> Prikazi </div>
                 </th>
-                <th scope="col" className="px-6 py-3 whitespace-nowrap flex justify-center">
-                  Vozaci
+                <th scope="col" className="px-6 py-3 whitespace-nowrap">
+                  <div className="flex justify-center"> Vozaci </div>
                 </th>
               </tr>
             </thead>
